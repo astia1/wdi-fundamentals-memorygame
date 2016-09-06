91d7c35187b2b65c20944a963fee7117c6ad8921
John McKinney <McKinney@Johns-Mac-Pro.local>
2016-09-06 15:01:42 -0700

    added alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,35 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo===cardFour) {
+  alert("You found a match!");}
+else      
+{alert ("Sorry, try again.");
+} 
+if (cardOne===cardThree) {
+  alert("You found a match!");}
+else      
+{alert ("Sorry, try again.");
+} 
+if (cardOne===cardTwo) {
+  alert("You found a match!");}
+else      
+{alert ("Sorry, try again.");
+} 
+if (cardOne===cardFour) {
+  alert("You found a match!");}
+else      
+{alert ("Sorry, try again.");
+} 
+if (cardThree===cardFour) {
+  alert("You found a match!");}
+else      
+{alert ("Sorry, try again.");
+} if (cardThree===cardTwo) {
+  alert("You found a match!");}
+else      
+{alert ("Sorry, try again.");
+}
+
+
